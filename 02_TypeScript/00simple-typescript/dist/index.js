3ac9fdc11bdd583489bcaaabca3b73696836e53f
Fabiola <you@example.com>
2022-01-18 13:29:16 -0300

    first commit

---
+++ 02_TypeScript/00simple-typescript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+const a = 5;
+const b = 10;
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
---
+++ 02_TypeScript/01Modules/src/index.ts
@@ -0,0 +1,8 @@
+import multiply, { multiplyByTwo as mBy2, HelloWorld } from './multiply';
+//import * as multiplyModule from './multiply';
+
+const a = 5;
+const b = 2;
+
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
+mBy2(2);
